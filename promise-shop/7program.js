1b24b772e472621d309df075afde0e19299a612f
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-17 10:32:24 +0530

    7program.js with few changes

--- promise-shop/7program.js
+++ promise-shop/7program.js
@@ -1,3 +1,7 @@
+const first=()=>{
+    return Promise.resolve('random value');
+}
+const second=(arg)=>arg+'2';
 let firstPromise = first();
 
 let secondPromise = firstPromise.then(val=>second(val));
@@ -6,3 +10,4 @@ secondPromise.then(val=>console.log(val));
 
 // As an alternative to the code above, you could also do this:
 // first().then(second).then(console.log);
+module.exports={firstPromise,secondPromise}
