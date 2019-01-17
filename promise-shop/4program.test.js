bef02594e1529f925f19e4e4ab5f57226154990c
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-17 09:53:14 +0530

    Changes in test cases for 4program.js

--- promise-shop/4program.test.js
+++ promise-shop/4program.test.js
@@ -1,10 +1,12 @@
-jest.useFakeTimers();
 const fn=require('./4program.js');
 describe('"4program.js"  prints "I FIRED!" to show that promises resolve only one time',()=>{
     it('should  print "I FIRED!" on resolving',()=>{
-        expect(fn()).resolves.toEqual('I FIRED');
+        expect.assertions(1);
+        return expect(fn).resolves.toEqual('I FIRED!');
     });
-    it('should not print "I DID NOT FIRE!" on rejecting',()=>{
-        expect(fn()).rejects.not.toEqual('I DID NOT FIRE!');
-    });
+    /*it('should  print "I FIRED!" on resolving',()=>{
+        expect.assertions(1);
+        return expect(fn).rejectes.toThrowError('I DID NOT FIRE!');
+    });*/
+    
 })
