a567b5aeb95e2a3a94e6b12a08af4f87b77f9417
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-17 09:52:58 +0530

    4program.js with few changes

--- promise-shop/4program.js
+++ promise-shop/4program.js
@@ -1,18 +1,16 @@
 require('es6-promise');
 
-let promises=()=>{
-    let promise = new Promise(function (fulfill, reject) {
-      // Your solution here
-      fulfill('I FIRED');
-      reject(new Error('I DID NOT FIRE'));
-    });
-    let onReject=(error)=>console.log(error.message);
 
-    promise.then(
-        result => console.log(result), 
-        error => onReject(error) 
-    );
-return promise;
-}
+const promise = new Promise(function (fulfill, reject) {
+     fulfill('I FIRED!')
+     reject(new Error('I DID NOT FIRE'));
+});
+let onReject=(error)=>console.log(error.message);
+
+promise.then(
+    result => console.log(result), 
+    error => onReject(error) 
+);
+
 //promises();
-module.exports=promises;
+module.exports=promise;
