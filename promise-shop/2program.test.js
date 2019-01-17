35fdf936e0f8091c0394142ae596e41f608407fe
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-17 09:43:22 +0530

    Changes in test cases for 2program.js

--- promise-shop/2program.test.js
+++ promise-shop/2program.test.js
@@ -1,21 +1,23 @@
-jest.useFakeTimers();
+
 const fn=require('./2program.js');
 describe('"2program.js" waits for 300ms and then prints "FULFILLED!"',()=>{
+    it('should print "FULFILLED!" on resolving',()=>{
+        expect.assertions(1);
+        return expect(fn.promise).resolves.toBe('FULFILLED!');
+    });
+    /*it('should not print "FULFILLED!" on rejecting',()=>{
+        return expect(fn()).rejects.toEqual('FULLFILLED!');
+    });*/
+})
+jest.useFakeTimers();
+describe('"2program.js" waits for 300ms and then prints "FULFILLED!"',()=>{
     it('if setTimeout() is called once ',()=>{
-        fn();
+        fn.check();
+       
         expect(setTimeout).toHaveBeenCalledTimes(1);
     });
     it('if setTimeout() is called with 300ms',()=>{
-        fn();
+        fn.check();
         expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 300);
-    });
-    it('should print "FULFILLED!" on resolving',()=>{
-        expect(fn()).resolves.toEqual('FULLFILLED!');
-        /*return fn().then((data) => {
-            expect(data).toBe('FULFILLED!');
-          });*/
-    });
-    it('should not print "FULFILLED!" on rejecting',()=>{
-        expect(fn()).rejects.not.toEqual('FULLFILLED!');
     });
 })
