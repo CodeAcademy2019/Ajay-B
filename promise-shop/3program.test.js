974376c06b5ee77f7af14ca9773b7ebd7afca71a
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-17 09:44:21 +0530

    Changes in test cases for 3program.js

--- promise-shop/3program.test.js
+++ promise-shop/3program.test.js
@@ -1,21 +1,23 @@
-jest.useFakeTimers();
+
 const fn=require('./3program.js');
 describe('"3program.js" waits for 300ms and then prints "REJECTED!"',()=>{
+    it('should print "REJECTED!" on rejecting',()=>{
+        expect.assertions(1);
+        return expect(fn.promise).rejects.toThrowError('REJECTED');
+    });
+    /*it('should not print "FULFILLED!" on rejecting',()=>{
+        return expect(fn()).rejects.toEqual('FULLFILLED!');
+    });*/
+})
+jest.useFakeTimers();
+describe('"3program.js" waits for 300ms and then prints "REJECTED!"',()=>{
     it('if setTimeout() is called once ',()=>{
-        fn();
+        fn.check();
+       
         expect(setTimeout).toHaveBeenCalledTimes(1);
     });
     it('if setTimeout() is called with 300ms',()=>{
-        fn();
+        fn.check();
         expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 300);
-    });
-    it('should not print "REJECTED!" on resolving',()=>{
-        expect(fn()).resolves.not.toEqual('FULLFILLED!');
-        /*return fn().then((data) => {
-            expect(data).toBe('FULFILLED!');
-          });*/
-    });
-    it('should print "REJECTED!" on rejecting',()=>{
-        expect(fn()).rejects.toEqual('REJECTED!');
     });
 })
