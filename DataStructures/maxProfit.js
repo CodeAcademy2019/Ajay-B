83594bdfd4caef937bd52b9878a0c15bcfa02eaa
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-20 13:04:04 +0530

    code that passes few more test cases

--- DataStructures/maxProfit.js
+++ DataStructures/maxProfit.js
@@ -15,7 +15,7 @@ let findMaxProfit=(stack)=>{
         }
     }
     console.log(index);
-    return [Math.max(...cumulativeProfit),...index];
+    return [Math.max(...cumulativeProfit),index];
 }
 const main=(testArray)=>{
     if (testArray[i]!==0){
@@ -25,22 +25,65 @@ const main=(testArray)=>{
         console.log(testArray[i]);
         for(j=i;j<i+testArray[i];j++){
             //console.log(testArray[j+1][0]);
-            [profit,indices]=findMaxProfit(testArray[j+1]);
-            maxProfitPerStack.push(profit);
-            index.push(indices);
+            maxProfitPerStack.push(findMaxProfit(testArray[j+1]));
+            //maxProfitPerStack.push(profit);
+            //index.push(indices);
         }
-        console.log('profit',maxProfitPerStack);
-        console.log('indices',index);
+        console.log('profit and no of boxes',maxProfitPerStack);
+        //console.log('indices',index);
         
         for(let i=0;i<maxProfitPerStack.length;i++){
-            if(maxProfitPerStack[i]<0){
-                index.splice(i,1)
+            if(maxProfitPerStack[i][0]<0){
+                maxProfitPerStack.splice(i,1)
             }
         }
-        maxProfitPerStack=maxProfitPerStack.filter(x=>x>=0);
-        console.log('profit',maxProfitPerStack);
-        console.log('indices',index);
-        return [maxProfitPerStack.reduce((x,y)=>x+y),index.reduce((x,y)=>x+y)];
+        //maxProfitPerStack=maxProfitPerStack.filter(x=>x>=0);
+        let comparator=(x,y)=>{
+            return y[0]-x[0];
+        }
+        maxProfitPerStack=maxProfitPerStack.sort(comparator);
+        
+        console.log('profit & no of boxes',maxProfitPerStack);
+        //console.log('indices',index);
+        let totalMaxProfit=0;
+        for(let i=0;i<maxProfitPerStack.length;i++){
+            totalMaxProfit+=maxProfitPerStack[i][0];
+        }
+        //totalMaxProfit=maxProfitPerStack.reduce((x,y)=>x[0]+y[0]);
+        let maxProfitTillNow=0;
+        
+        let totalNoOfBoxes=maxProfitPerStack[0][1].length;
+        for(let i=1;i<maxProfitPerStack.length;i++){
+            totalNoOfBoxes*=maxProfitPerStack[i][1].length;
+        }
+        let noOfBoxesTillNow=new Array(totalNoOfBoxes);
+        //noOfBoxesTillNow=totalNoOfBoxes.map(x=>0);
+        console.log(totalNoOfBoxes);
+        for (let i=0;i<totalNoOfBoxes;i++){
+            noOfBoxesTillNow[i]=0;
+        }
+        //console.log('j',maxProfitPerStack[0][1][0])
+        //console.log(noOfBoxesTillNow);;
+        for(let i=0;i<maxProfitPerStack.length;i++){
+            //maxProfitTillNow+=maxProfitPerStack[i][0];
+            let k=0;
+            let j=totalNoOfBoxes/maxProfitPerStack[i][1].length;
+            console.log('j',j);
+            let m=0;
+            let n=j;
+            for(k=0;k<maxProfitPerStack[i][1].length;k++){
+                for(m;m<n;m++){
+                    noOfBoxesTillNow[m]+=maxProfitPerStack[i][1][k];
+                    //j=j+1;
+                }
+                m=m+j-1;
+                n=n+j;
+                console.log(noOfBoxesTillNow);
+            }
+        }
+        
+        console.log('Boxes',noOfBoxesTillNow);
+        return [totalMaxProfit,noOfBoxesTillNow];
     }
     else{
         console.log('No more input');
@@ -48,5 +91,16 @@ const main=(testArray)=>{
     }
 }
 //console.log(main([2,[1,2,3],[1,2,3]]));
-console.log(main([4,[16,14,12,15,17,18,1,1,1,1],[20,20,10,10,8,18,17,1,1,1],[1, 2, 3, 4, 15, 14, 13, 12, 11, 10, 20, 19, 18, 17, 16],[ 1, 1, 18, 17, 1]]));
+/*2
+3
+3 10 10 1
+5 10 1 10 1 10
+4 5 15 10 5
+*/
+
+//console.log(main([4,[16,14,12,15,17,18,1,1,1,1],[20,20,10,10,8,18,17,1,1,1],[1, 2, 3, 4, 15, 14, 13, 12, 11, 10, 20, 19, 18, 17, 16],[ 1, 1, 18, 17, 1]]));
+//console.log(main([3,[4,6 ,7 ,12, 15, 4, 11],[5, 9, 8, 12, 6, 7, 14, 6, 10],[10, 5 ,6 ,7 ,12, 17, 10, 8, 12 ,10 ,9]]));
+//console.log(main([3,[19,9,2],[20,11,10,7,1],[18,2]]));
+//Hermanth:console.log(main([3,[19 ,9 ,2],[20,11,10,7,1],[18 ,2]]));
+console.log(main([3,[10,10,1],[10 ,1, 10, 1, 10],[5,15, 10, 5]]));
 module.exports=main;
--- DataStructures/maxProfit.test.js
+++ DataStructures/maxProfit.test.js
@@ -1,13 +1,26 @@
 const maxProfit= require('./maxProfit.js');
 describe('maxProfit', () => {
-    it('should return maximum profit and the number of boxes required to gain maximum profit', () => {
-        expect(maxProfit([4,[16,14,12,15,17,18,1,1,1,1],[20,20,10,10,8,18,17,1,1,1],[1, 2, 3, 4, 15, 14, 13, 12, 11, 10, 20, 19, 18, 17, 16],[ 1, 1, 18, 17, 1]])).toEqual([52,16]);
+    it('Manish:should return maximum profit and the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([4,[16,14,12,15,17,18,1,1,1,1],[20,20,10,10,8,18,17,1,1,1],[1, 2, 3, 4, 15, 14, 13, 12, 11, 10, 20, 19, 18, 17, 16],[ 1, 1, 18, 17, 1]])).toEqual([52,[16]]);
     });
-    it('should return maximum profit', () => {
-        expect(maxProfit([3,[3,2,1,3,10,12,2],[12, 13, 14 ,15, 2 ,3],[1, 2, 3]])).toEqual([62,16]);
+    it('Ashrene:should return maximum profit and the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([3,[3,2,1,3,10,12,2],[12, 13, 14 ,15, 2 ,3],[1, 2, 3]])).toEqual([62,[16]]);
     });
-    it('should return maximum profit', () => {
-        expect(maxProfit([2,[8,1,9,15, 3, 6, 10],[7, 2, 14, 10]])).toEqual([29,8]);
+    it('Gurehmat:should return maximum profit and the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([2,[8,1,9,15, 3, 6, 10],[7, 2, 14, 10]])).toEqual([29,[8,9]]);
     });
+    it('Shashwath:should return maximum profit and all possible values of the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([3,[4,6 ,7 ,12, 15, 4, 11],[5, 9, 8, 12, 6, 7, 14, 6, 10],[10, 5 ,6 ,7 ,12, 17, 10, 8, 12 ,10 ,9]])).toEqual([ 38, [ 13, 15, 16 ] ]);
+    });
+    it('Gurehmat:should return maximum profit and all possible values of the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([1,[5 ,13 ,8 ,7 ,11, 9, 2, 10, 11, 13]])).toEqual([15,[7,8]]);
+    });
+    xit('Hemanth:should return maximum profit and all possible values of the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([3,[19 ,9 ,2],[20,11,10,7,1],[18 ,2]])).toEqual([1,[5,7,8,10]]);
+    });
+    xit('Yash:should return maximum profit and all possible values of the number of boxes required to gain maximum profit', () => {
+        expect(maxProfit([2,[1,2,3 ,10, 15, 5],[15 ,5 ,15 ,5 ,15, 5]])).toEqual([24, [3 ,4 ,6 ,8 ,10, 12]]);
+    });
+
     
 });
