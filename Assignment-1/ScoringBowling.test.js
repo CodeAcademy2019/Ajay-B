0e1d1c00f490753035bfaf940adf71017991d5c5
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-22 09:57:40 +0530

    testing for the valid number of inputs

--- Assignment-1/ScoringBowling.test.js
+++ Assignment-1/ScoringBowling.test.js
@@ -1,8 +1,11 @@
 const scoreBowling=require('./ScoringBowling');
 
 describe('testInput',()=>{
-    it('should return Invalid number of frames if the game is not yet finished(that is frame length<10)',()=>{
+    it('should return number of frames if the game is finished(that is frame length=10)',()=>{
         expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6)).toEqual(10);
+    });
+    it('should return "Invalid number of frames" if the game is not yet finished(that is frame length<10)',()=>{
+        expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3)).toEqual('Invalid number of frames');
     });
 
 });
