6f043439c999a2048c3585d4f74add8e91928d0f
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-22 12:14:54 +0530

    formatted code and added few test cases

--- Assignment-1/ScoringBowling.js
+++ Assignment-1/ScoringBowling.js
@@ -1,66 +1,60 @@
-let validateInput=(...args)=>{
-    if( args.filter(x=>x>10).length===0){
-        return true;
-    }else{
-        return false;
-    }
-}
-let isStrike=(first)=>{
-    if (first===10){
-        return true;
-    }
-    return false;
-}
-let isSpare=(first,second)=>{
-    if(first+second===10){
-        return true;
-    }
-    return false;
-}
-let setOfValuesPerFrame=(...inputs)=>{
-    if(isStrike(inputs[0])){
-        return ([[inputs[0],inputs[1],inputs[2]],0]);
-    }
-    else if(isSpare(inputs[0],inputs[1])){
-        return ([[inputs[0],inputs[1],inputs[2]],1]);
-    }
-    else{
-        return ([[inputs[0],inputs[1]],1]);
-    }
-    
-}
-let score=(scoreFrames)=>{
-    scoreFrames=scoreFrames.filter(x=>x.includes(undefined)!==true);
-    return scoreFrames.map(x=>x.reduce((a,b)=>a+b,0)).reduce((x,y)=>x+y,0);
-   
-}
+const validateInput = (...args) => {
+  if (args.filter(x => x > 10).length === 0) {
+    return true;
+  }
+  return false;
+};
+const isStrike = (first) => {
+  if (first === 10) {
+    return true;
+  }
+  return false;
+};
+const isSpare = (first, second) => {
+  if (first + second === 10) {
+    return true;
+  }
+  return false;
+};
+const setOfValuesPerFrame = (...inputs) => {
+  if (isStrike(inputs[0])) {
+    return ([[inputs[0], inputs[1], inputs[2]], 0]);
+  } if (isSpare(inputs[0], inputs[1])) {
+    return ([[inputs[0], inputs[1], inputs[2]], 1]);
+  }
+  return ([[inputs[0], inputs[1]], 1]);
+};
+const score = (scoreFrames) => {
+  scoreFrames = scoreFrames.filter(x => x.includes(undefined) !== true);
+  return scoreFrames.map(x => x.reduce((a, b) => a + b, 0)).reduce((x, y) => x + y, 0);
+};
 
-let rolls=(...allRolls)=>{
-    let scoreFrames=[];
-    if(validateInput(...allRolls)){
-        let i=0;
-        for(i=0;i<allRolls.length;i++){
-            let framesAndSkip=setOfValuesPerFrame(allRolls[i],allRolls[i+1],allRolls[i+2],allRolls[i+3]);
-            scoreFrames.push(framesAndSkip[0]);
-            i+=framesAndSkip[1];
-        }
-        return scoreFrames;
-    }else{
-        return 'Invalid input';
-    }
-    
-}
-let main=(...args)=>{
-    let scoreFrames=rolls(...args);
-    if(scoreFrames!=='Invalid input'){
-        return(score(scoreFrames)); 
+const rolls = (...allRolls) => {
+  const scoreFrames = [];
+  if (validateInput(...allRolls)) {
+    let i = 0;
+    for (i = 0; i < allRolls.length; i++) {
+      const framesAndSkip = setOfValuesPerFrame(allRolls[i], allRolls[i + 1], allRolls[i + 2], allRolls[i + 3]);
+      scoreFrames.push(framesAndSkip[0]);
+      i += framesAndSkip[1];
     }
     return scoreFrames;
-}    
-//console.log(setOfValuesPerFrame(10,10,2,3));
-/*console.log(main(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10));
+  }
+  return 'Invalid input';
+};
+const main = (...args) => {
+  const scoreFrames = rolls(...args);
+  if (scoreFrames !== 'Invalid input') {
+    return (score(scoreFrames));
+  }
+  return scoreFrames;
+};
+// console.log(setOfValuesPerFrame(10,10,2,3));
+console.log(main(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10));
 console.log(main(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6));
 console.log(main(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10));
 console.log(main(6, 4, 3, 0, 10, 10,5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-console.log(rolls(13,14));*/
-module.exports={validateInput,isStrike,isSpare,setOfValuesPerFrame,score,rolls,main};
+console.log(main(13,14)); 
+module.exports = {
+  validateInput, isStrike, isSpare, setOfValuesPerFrame, score, rolls, main,
+};
--- Assignment-1/ScoringBowling.test.js
+++ Assignment-1/ScoringBowling.test.js
@@ -1,85 +1,87 @@
-const scoreBowling=require('./ScoringBowling');
+const scoreBowling = require('./ScoringBowling');
 
-describe('validateInput',()=>{
-    it('should return "true" if the game has valid inputs',()=>{
-        expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6)).toEqual(true);
-    });
+describe('validateInput', () => {
+  it('should return "true" if the game has valid inputs', () => {
+    expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6)).toEqual(true);
+  });
 
-    it('should return "false" if the game has invalid inputs',()=>{
-        expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 13)).toEqual(false);
-    });
-
+  it('should return "false" if the game has invalid inputs', () => {
+    expect(scoreBowling.validateInput(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 13)).toEqual(false);
+  });
 });
 
-describe('isStrike',()=>{
-    it('should return "true" if the frame is a strike',()=>{
-        expect(scoreBowling.isStrike(10)).toEqual(true);
-    });
-    it('should return "false" if the frame is not a strike',()=>{
-        expect(scoreBowling.isStrike(6)).toEqual(false);
-    });
-})
-describe('isSpare',()=>{
-    it('should return "true" if the frame is a spare',()=>{
-        expect(scoreBowling.isSpare(9,1)).toEqual(true);
-    });
-    it('should return "false" if the frame is not a spare',()=>{
-        expect(scoreBowling.isSpare(6,3)).toEqual(false);
-    });
-})
-describe('setOfValuesPerFrame',()=>{
-    it('should return the array of scores that are to be considerd while finding the total score of one frame',()=>{
-        expect(scoreBowling.setOfValuesPerFrame(10,10,2,3)[0]).toEqual([10,10,2]);
-    })
-    it('should return the array of scores that are to be considerd while finding the total score of one frame',()=>{
-        expect(scoreBowling.setOfValuesPerFrame(9,1,2,3)[0]).toEqual([9,1,2]);
-    })
-    it('should not return anything other than the array of scores that are to be considerd while finding the total score of one frame',()=>{
-        expect(scoreBowling.setOfValuesPerFrame(9,1,2,3)[0]).not.toEqual([9,1,2,3]);
-    })
-    
+describe('isStrike', () => {
+  it('should return "true" if the frame is a strike', () => {
+    expect(scoreBowling.isStrike(10)).toEqual(true);
+  });
+  it('should return "false" if the frame is not a strike', () => {
+    expect(scoreBowling.isStrike(6)).toEqual(false);
+  });
+});
 
-})
+describe('isSpare', () => {
+  it('should return "true" if the frame is a spare', () => {
+    expect(scoreBowling.isSpare(9, 1)).toEqual(true);
+  });
+  it('should return "false" if the frame is not a spare', () => {
+    expect(scoreBowling.isSpare(6, 3)).toEqual(false);
+  });
+});
 
-describe('rolls',()=>{
-    it('should return the array of scores that are to be considerd while finding the total score of all frames',()=>{
-        expect(scoreBowling.rolls(10,10,2,3)).toEqual([[10,10,2],[10,2,3],[2,3]]);
-    })
-    it('should return the array of scores that are to be considerd while finding the total score of all frames',()=>{
-        expect(scoreBowling.rolls(9,1,2,3)).toEqual([[9,1,2],[2,3]]);
-    })
-    it('should not return anything other than the array of scores that are to be considerd while finding the total score of allframes',()=>{
-        expect(scoreBowling.rolls(9,1,2,3)).not.toEqual([[9,1,2]]);
-    })
-    it('should  return "Invalid input" if the input is invalid ',()=>{
-        expect(scoreBowling.rolls(19,1,2,3)).toEqual('Invalid input');
-    })
-    
+describe('setOfValuesPerFrame', () => {
+  it('should return the array of scores that are to be considerd while finding the total score of one frame', () => {
+    expect(scoreBowling.setOfValuesPerFrame(10, 10, 2, 3)[0]).toEqual([10, 10, 2]);
+  });
+  it('should return the array of scores that are to be considerd while finding the total score of one frame', () => {
+    expect(scoreBowling.setOfValuesPerFrame(9, 1, 2, 3)[0]).toEqual([9, 1, 2]);
+  });
+  it('should not return anything other than the array of scores that are to be considerd while finding the total score of one frame', () => {
+    expect(scoreBowling.setOfValuesPerFrame(9, 1, 2, 3)[0]).not.toEqual([9, 1, 2, 3]);
+  });
+});
 
-})
-describe('score',()=>{
-    it('should return the total score of all frames',()=>{
-        expect(scoreBowling.score([[6,4,3],[3,0],[10,0,0]])).toEqual(26)
-    })
-    it('should not return anything other than return the total score of all frames',()=>{
-        expect(scoreBowling.score([[6,4,3],[3,0],[10,0,0]])).not.toEqual(25)
-    })
-})
+describe('rolls', () => {
+  it('should return the array of scores that are to be considerd while finding the total score of all frames', () => {
+    expect(scoreBowling.rolls(10, 10, 2, 3)).toEqual([[10, 10, 2], [10, 2, 3], [2, 3]]);
+  });
+  it('should return the array of scores that are to be considerd while finding the total score of all frames', () => {
+    expect(scoreBowling.rolls(9, 1, 2, 3)).toEqual([[9, 1, 2], [2, 3]]);
+  });
+  it('should not return anything other than the array of scores that are to be considerd while finding the total score of allframes', () => {
+    expect(scoreBowling.rolls(9, 1, 2, 3)).not.toEqual([[9, 1, 2]]);
+  });
+  it('should  return "Invalid input" if the input is invalid ', () => {
+    expect(scoreBowling.rolls(19, 1, 2, 3)).toEqual('Invalid input');
+  });
+});
 
-describe('main',()=>{
-    it('should return the total final score of the bowling',()=>{
-        expect(scoreBowling.main(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10)).toEqual(300);
-    })
-    it('should return the total final score of the bowling',()=>{
-        expect(scoreBowling.main(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6)).toEqual(90);
-    })
-    it('should return the total final score of the bowling',()=>{
-        expect(scoreBowling.main(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10)).toEqual(30);
-    })
-    it('should not return anything other than the total final score of the bowling',()=>{
-        expect(scoreBowling.main(6, 4, 3, 0, 10, 10,5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)).not.toEqual(68);
-    })
-    it('should  return "Invalid input" if the input is invalid ',()=>{
-        expect(scoreBowling.main(19,1,2,3)).toEqual('Invalid input');
-    })
-})
+describe('score', () => {
+  it('should return the total score of all frames', () => {
+    expect(scoreBowling.score([[6, 4, 3], [3, 0], [10, 0, 0]])).toEqual(26);
+  });
+  it('should not return anything other than return the total score of all frames', () => {
+    expect(scoreBowling.score([[6, 4, 3], [3, 0], [10, 0, 0]])).not.toEqual(25);
+  });
+});
+
+describe('main', () => {
+  it('should return the total final score of the bowling', () => {
+    expect(scoreBowling.main(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)).toEqual(300);
+  });
+  it('should return the total final score of the bowling', () => {
+    expect(scoreBowling.main(3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6)).toEqual(90);
+  });
+  it('should return the total final score of the bowling', () => {
+    expect(scoreBowling.main(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10)).toEqual(30);
+  });
+  it('should return the total final score of the bowling', () => {
+    expect(scoreBowling.main(6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)).toEqual(16);
+  });
+  //6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  it('should not return anything other than the total final score of the bowling', () => {
+    expect(scoreBowling.main(6, 4, 3, 0, 10, 10, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)).not.toEqual(68);
+  });
+  it('should  return "Invalid input" if the input is invalid ', () => {
+    expect(scoreBowling.main(19, 1, 2, 3)).toEqual('Invalid input');
+  });
+});
